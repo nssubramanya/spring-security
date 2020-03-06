8f97660f8b41b6dd28448f8b46b66774977d212e
Subramanya N S <subramanya.ns@gmail.com>
2020-03-06 11:22:26 +0530

    m1-l3: Authorization rules based on paths (/delete)

--- spring-security/src/main/java/com/subbu/springsecdemo/config/SpringSecurityConfig.java
+++ spring-security/src/main/java/com/subbu/springsecdemo/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.subbu.springsecdemo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,9 +12,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().
-			withUser("user").
-			password("{noop}pass").
-			roles("USER");
+		auth.inMemoryAuthentication().withUser("user").password("{noop}pass").roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeRequests().antMatchers("/users/delete/**").hasAuthority("ADMIN")
+			.anyRequest().authenticated()
+			.and().formLogin()
+			.and().httpBasic()
+			.and().exceptionHandling().accessDeniedPage("/accessDenied.html");
+		// @formatter:on
 	}
 }
